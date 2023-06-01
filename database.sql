9fca402e90e50770d82e3d08320a72e11a1a5f2e
Mari Vaypan <avemari98@gmail.com>
2023-06-01 17:28:46 +0300

    repository initialisation

---
+++ database.sql
@@ -0,0 +1,67 @@
+CREATE DATABASE InvitingList;
+
+CREATE TABLE friendslist 
+(
+	person_id serial PRIMARY KEY,
+	name varchar(128) NOT NULL,
+	pic text NOT NULL 
+)
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Paul Black', 'https://img.freepik.com/free-photo/waist-up-portrait-handsome-serious-unshaven-male-keeps-hands-together-dressed-dark-blue-shirt-has-talk-with-interlocutor-stands-against-white-wall-self-confident-man-freelancer_273609-16320.jpg?t=st=1681845876~exp=1681846476~hmac=b717d86f7e5743e5f2c9aae4c8e1edb7523a071f71393cc6f201b4593974d33e');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Jack Stoll', 'https://img.freepik.com/free-photo/young-businessman-siting-cafeteria-with-laptop-computer-table_342744-455.jpg?w=996&t=st=1681845976~exp=1681846576~hmac=459d4d3c719b6f17dceefbbed391a0fd69e91db87a2e0818048ab27ac0e01526');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Kaan Archer', 'https://images.pexels.com/photos/3051652/pexels-photo-3051652.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Jenny Curran', 'https://img.freepik.com/free-photo/positive-carefree-european-woman-dances-with-rhythm-music-listens-favorite-song-via-headphones-wears-knitted-orange-sweater-smiles-broadly-isolated-purple-background-foolishes-around_273609-58571.jpg?w=996&t=st=1681845876~exp=1681846476~hmac=2b555bee01d67895b4c15336f5e75c4b27855c06fcfe18e6b084c3edd6d93339');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Oskar Madden', 'https://img.freepik.com/premium-photo/young-attractive-man-with-paper-cup-coffee-car_152898-4130.jpg');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Monica Beam', 'https://img.freepik.com/free-photo/portrait-good-looking-cheerful-african-american-woman-enjoys-company-small-pedigree-dog-wears-orange-jumper-spends-free-time-with-favorite-pet-isolated-white-wall-animal-owner_273609-49840.jpg?w=996&t=st=1681845905~exp=1681846505~hmac=bcac9b623ef42cc7bf7e4e2bf1587627dcc470a18f4be5ef4c6b9476b39cb305');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Leo Finch', 'https://img.freepik.com/free-photo/caucasian-pretty-girl-walking-around-city-with-boyfriend-smiling-brunette-man-spending-weekend-with-girlfriend_197531-12261.jpg');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Emma Smith', 'https://img.freepik.com/free-photo/beautiful-female-office-worker-carrying-out-administrative-work-company_197531-4960.jpg?w=996&t=st=1681846127~exp=1681846727~hmac=1148ee6c605af034f5e69864589b48ba58a9d58f5942ca7dba6a28988923a5b1');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Henry Johnson', 'https://img.freepik.com/free-photo/guy-resting-his-head-his-hands-metal-staircase-yellow-door_181624-45375.jpg?t=st=1681846052~exp=1681846652~hmac=9415575fca03c43b542d80cff60b5da2feaeba94a9af39e7474def43815586b1');
+
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Sam Miller', 'https://img.freepik.com/premium-photo/cheerful-smiling-man-casual-attire-is-feeling-happy-when-holding-his-adorable-dog-york-terrier_245974-10637.jpg?w=360');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Liam Bush', 'https://img.freepik.com/free-photo/cool-guy-shows-his-finger-up-listens-music-orange-background-portrait-darkskinned-man-blue-tee-dancing-isolated-backdrop_197531-28583.jpg?w=996&t=st=1681846111~exp=1681846711~hmac=575b88311ac9d7efa025f30c6f0febe20cb2e34d479091ccc0adbf08ca1a304c');
+
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Katy Dyer', 'https://images.pexels.com/photos/14517588/pexels-photo-14517588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Lester Castaneda', 'https://images.pexels.com/photos/792326/pexels-photo-792326.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Emilio Cross', 'https://images.pexels.com/photos/2896853/pexels-photo-2896853.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Kirsten Winter', 'https://images.pexels.com/photos/774095/pexels-photo-774095.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('Gina Dickerson', 'https://images.pexels.com/photos/2313635/pexels-photo-2313635.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+INSERT INTO friendslist("name", "pic")
+VALUES ('James Guerra', 'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2');
+
+
+
---
+++ index.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+//middleware
+app.use(cors());
+app.use(express.json()); //req.body
+
+//ROUTES
+
+//test route 
+app.get('/', async (req, res) => {
+    try {
+        res.status(200).json({message: 'working!!!'})
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//get all friends list DONE
+app.get('/friends-list', async (req, res) => {
+    try {
+        const allFriends = await pool.query('SELECT * FROM friendslist');
+        res.json(allFriends.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+//create an event
+app.post('/create-new-inviting-list', async (req, res) => {
+    const eventData = req.body;
+    const eventDataModified = 'invitinglist_' + String(eventData.eventName) + '_' + String(eventData.eventDate);
+    try {
+        const createNewTable = await pool.query(`
+            CREATE ${eventDataModified} (person_id, name, pic)
+        `)
+
+    } catch (error) {
+        console.error(error.message);
+    }
+});
+
+//get list of all events
+app.get('/get-list-of-tables', async (req, res) => {
+    try {
+        const getListOfTables = await pool.query(`
+                SELECT tablename
+                FROM pg_catalog.pg_tables
+                WHERE schemaname = 'public'
+        `)
+
+        res.json(getListOfTables);
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+//create an inviting list with date and event name (name type: "invitinglist_eventname_2023-05-23")
+//get friends from inviting list DONE
+app.get('/inviting-list', async (req, res) => {
+    try {
+        const allInvitedFriends = await pool.query('SELECT * FROM invitinglist');
+        res.json(allInvitedFriends.rows);
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
+
+//add a specific person from friendslist to invitinglist DONE
+app.put('/move-to-inviting-list/:person_id', async (req, res) => {
+    try {
+        const { person_id, name, pic }  = req.body;
+        const personId = req.params.person_id;
+
+        const moveToInvitingList = await pool.query(`
+            INSERT INTO invitinglist (person_id, name, pic)   
+            SELECT person_id, name, pic   
+            FROM friendslist  
+            WHERE person_id = ${personId} AND person_id IS NOT NULL AND name IS NOT NULL AND pic IS NOT NULL;
+            `);
+
+        const deleteFromFriendsList = await pool.query(`DELETE FROM friendslist WHERE person_id =${personId}
+        `);       
+
+        res.status(200).json({ message: 'Friend moved to Inviting List successfully' });
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+//delete a specific person fronm inviting list and move them to friendslist DONE
+app.put('/move-to-friends-list/:person_id', async (req, res) => {
+    try {
+        const personId = req.params.person_id;
+
+        const moveToFriendsList = await pool.query(`
+        INSERT INTO friendslist (person_id, name, pic)
+        SELECT person_id, name, pic
+        FROM invitinglist
+        WHERE person_id=${personId} AND person_id IS NOT NULL AND name IS NOT NULL AND pic IS NOT NULL;
+        `);
+        const deleteFromInvitingList = await pool.query(`
+        DELETE FROM invitinglist 
+        WHERE person_id =${personId}
+        `);
+
+        res.status(200).json({ message: 'Friend moved to Friends List successfully' });
+        
+    } catch (error) {
+        console.error(error.message)
+    }
+    
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
