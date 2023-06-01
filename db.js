99c83d0518dceb9215816dcc386caf9953953609
Mari Vaypan <avemari98@gmail.com>
2023-06-01 18:59:16 +0300

    add versel settings

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'catdevil',
-    host: 'localhost',
-    port: 5433,
-    database: 'InvitingList'
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require"
 });
 
 module.exports = pool;
