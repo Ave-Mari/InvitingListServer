726449385354a21cd53a180e16aae2e3b3393b2c
Mari Vaypan <avemari98@gmail.com>
2023-06-01 22:56:10 +0300

    add function to connect pool

--- db.js
+++ db.js
@@ -4,4 +4,12 @@ const pool = new Pool({
     connectionString: process.env.POSTGRES_URL + "?sslmode=require"
 });
 
+pool.connect((err) => {
+    if (err) {
+        console.error('Error: ', err.message)
+    }
+    console.log("Connect to PostgreSQL successfully!")
+})
+
+
 module.exports = pool;
