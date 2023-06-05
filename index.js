bd85d0a1ed8222c8327c893c8ab1197c2c190caa
Mari Vaypan <avemari98@gmail.com>
2023-06-05 17:19:53 +0300

    move pool connection to index.js

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const pool = require("./db");
+const { Pool } = require('pg');
 
 require('dotenv').config();
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
+});
+
+pool.connect((err) => {
+    if (err) {
+        console.error('Error: ', err.message)
+    } else {
+        console.log("Connect to PostgreSQL successfully!")
+    }
+    
+})
 
 //middleware
 app.use(cors());
