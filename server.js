c8ead730580fec4feb5af1f0e900f8e78ea53107
Mari Vaypan <avemari98@gmail.com>
2023-06-05 19:26:54 +0300

    rename index.js

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const cors = require("cors");
 require('dotenv').config();
 
 app.use(cors());
-// app.use(bodyParser.json()); 
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 const listsRouter = require('./routes/lists.router');
 
-app.use('/api/lists', listsRouter);
+app.use('/api', listsRouter);
 
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("Server is running on port 5000");
+    console.log(`Server is running on port ${PORT}`);
 });
