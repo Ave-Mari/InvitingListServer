1cd4c3851073de65f7485abebcf423dbf863fa74
Mari Vaypan <avemari98@gmail.com>
2023-06-05 18:54:12 +0300

    remove test code

--- index.js
+++ index.js
@@ -1,35 +1,22 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require('dotenv').config();
 
-app.listen(process.env.PORT || 5000);
-
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
-  })
-
-  module.exports = app
-
-// const bodyParser = require("body-parser")
-// const cors = require("cors");
-
-// require('dotenv').config();
-
-// //middleware
-// app.use(cors());
-// app.use(bodyParser.json()); 
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// const listsRouter = require('./routes/lists.router');
-
-// app.use('/api/lists', listsRouter);
-
-// const PORT = process.env.PORT || 5000
+app.use(cors());
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
-// app.listen(PORT, () => {
-//     console.log("Server is running on port 5000");
-// });
+const listsRouter = require('./routes/lists.router');
+
+app.use('/api/lists', listsRouter);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 5000");
+});
