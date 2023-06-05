8f9f0e1a47c36b29edbdc78c5d1f0567cf9cd29a
Mari Vaypan <avemari98@gmail.com>
2023-06-05 18:05:14 +0300

    add router to index

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json()); //req.body
 
+const listsRouter = require('./routes/lists.router');
+
+app.use('/api/lists', listsRouter);
+
 const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT, () => {
     console.log("Server is running on port 5000");
