ce07a5e91e83cb44308867fb84577766bc8445a0
Mari Vaypan <avemari98@gmail.com>
2023-06-05 17:10:09 +0300

    change Pool settings

--- db.js
+++ db.js
@@ -1,8 +1,15 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL + "?sslmode=require"
+    connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE
 });
+
+
 
 pool.connect((err) => {
     if (err) {
