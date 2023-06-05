52b5a4d9d0ad9cdda2842d08558d3747be5849cf
Mari Vaypan <avemari98@gmail.com>
2023-06-05 18:46:24 +0300

    test

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser")
-const cors = require("cors");
 
 require('dotenv').config();
 
-//middleware
-app.use(cors());
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(process.env.PORT || 5000);
 
-const listsRouter = require('./routes/lists.router');
+app.use(express.static('public'))
 
-app.use('/api/lists', listsRouter);
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.join(__dirname, 'public')});
+  })
 
-const PORT = process.env.PORT || 5000
+  module.exports = app
+
+// const bodyParser = require("body-parser")
+// const cors = require("cors");
+
+// require('dotenv').config();
+
+// //middleware
+// app.use(cors());
+// app.use(bodyParser.json()); 
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// const listsRouter = require('./routes/lists.router');
+
+// app.use('/api/lists', listsRouter);
+
+// const PORT = process.env.PORT || 5000
 
 
-app.listen(PORT, () => {
-    console.log("Server is running on port 5000");
-});
+// app.listen(PORT, () => {
+//     console.log("Server is running on port 5000");
+// });
